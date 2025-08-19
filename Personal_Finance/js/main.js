373135a7669b31596e1dc3007006f825430e06aa
KuijperDev <kuijperdev@outlook.com>
2025-08-19 16:56:31 +0200

    asd

--- Personal_Finance/js/main.js
+++ Personal_Finance/js/main.js
@@ -180,7 +180,7 @@ async function initApp(user) {
 
   const exportBtn = document.getElementById('export-csv-btn');
   if (exportBtn) {
-    exportBtn.addEventListener('click', async () => exportToCSV(userId));
+    exportBtn.addEventListener('click', () => exportToCSV(userId));
   }
 
   const importBtn = document.getElementById('import-xlsx-btn');
